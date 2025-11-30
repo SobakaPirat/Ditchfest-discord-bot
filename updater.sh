a67acb955794820a49ac282dd0d19677043cd205
Soba <egor.pudov@bk.ru>
2025-11-30 20:10:44 +0300

    separate updater script

---
+++ updater.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+while true; do
+    current_hour=$(date +%H)
+    current_minute=$(date +%M)
+    
+    # Если сейчас 5:00
+    if [ "$current_hour" = "05" ] && [ "$current_minute" = "00" ]; then
+        python updater.py >> logs/updater.log 2>&1
+        # Ждем 1 минуту чтобы не выполнить дважды в одну минуту
+        sleep 60
+    else
+        # Ждем 1 минуту и проверяем снова
+        sleep 60
+    fi
+done
