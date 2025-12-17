c5d60e542e93aff075af9c696f57c118830e465b
JassonStatham <egor.pudov@bk.ru>
2025-12-17 21:57:13 +0300

    remove bash script

--- updater.sh
+++ updater.sh
@@ -6,6 +6,8 @@ while true; do
     
     # Если сейчас 5:00
     if [ "$current_hour" = "05" ] && [ "$current_minute" = "00" ]; then
+        python3 maps_updater.py
+        python3 records_updater.py
         # Ждем 1 минуту чтобы не выполнить дважды в одну минуту
         sleep 60
     else
